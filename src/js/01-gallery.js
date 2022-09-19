f03154e895ea05176be44f75593de404e35de254
AlexAndriushchenko <aleksandr.work3@gmail.com>
2022-09-19 18:46:41 +0100

    all

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galeryContainer = document.querySelector('.gallery');
+galeryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGaleryItems(galleryItems)
+);
+
+function createGaleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}" />
+      </a> `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  animationSpeed: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('[name="email"]');
+const inputMessage = document.querySelector('[name="message"]');
+
+form.addEventListener('input', throttle(onMessage, 500));
+
+let formData = {};
+
+updateOutput();
+
+function onMessage(event) {
+  event.preventDefault();
+  formData = { email: inputEmail.value, message: inputMessage.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const savedData = localStorage.getItem('feedback-form-state');
+  console.log(savedData);
+  form.reset();
+  localStorage.clear();
+});
+
+function updateOutput() {
+  const saveData = localStorage.getItem('feedback-form-state');
+  const parseData = JSON.parse(saveData);
+
+  if (saveData) {
+    inputEmail.value = parseData.email || '';
+    inputMessage.value = parseData.message || '';
+  }
+}
